b03b5af22e8aa8ed9915420bde1e508c7254c22e
shulin <shulin_wu@yhaoo.com.tw>
2025-04-27 16:30:25 +0800

    備註

--- 0427.py
+++ 0427.py
@@ -1,33 +1,33 @@
-from mimetypes import inited
-
-from selenium import webdriver
-import os
-import requests
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-from soupsieve.css_parser import PSEUDO_COMPLEX_NO_MATCH
-
-url ='https://www.nike.com/tw/'
-driver = webdriver.Chrome()
-
-driver.get(url)
-htmlfile = driver.page_source
-
-soup = BeautifulSoup(htmlfile, 'html.parser')
-
-imgs = soup.find_all('img')
-# imgs=driver.find_elements(By.TAG_NAME,'img')
-# print(imgs)
-i=1
-for img in imgs:
-    path=img['data-landscape-url']
-    name = img['alt']
-    source=requests.get(path)
-    img_source=source.content
-    os.makedirs('images',exist_ok=True)
-    with open(f'images/{name}--{i}.jpg','wb') as f:
-        f.write(img_source)
-    i+=1
+# from mimetypes import inited
+#
+# from selenium import webdriver
+# import os
+# import requests
+# from bs4 import BeautifulSoup
+# from selenium.webdriver.common.by import By
+# from soupsieve.css_parser import PSEUDO_COMPLEX_NO_MATCH
+#
+# url ='https://www.nike.com/tw/'
+# driver = webdriver.Chrome()
+#
+# driver.get(url)
+# htmlfile = driver.page_source
+#
+# soup = BeautifulSoup(htmlfile, 'html.parser')
+#
+# imgs = soup.find_all('img')
+# # imgs=driver.find_elements(By.TAG_NAME,'img')
+# # print(imgs)
+# i=1
+# for img in imgs:
+#     path=img['data-landscape-url']
+#     name = img['alt']
+#     source=requests.get(path)
+#     img_source=source.content
+#     os.makedirs('images',exist_ok=True)
+#     with open(f'images/{name}--{i}.jpg','wb') as f:
+#         f.write(img_source)
+#     i+=1
 
 # 版本控制
 # github.com
@@ -53,3 +53,27 @@ for img in imgs:
 # git reflog (查看詳細記錄)
 # git reset 版本號 --hard (硬回復)
 # git reset 版本號 --soft (硬回復)
+
+# --git remote add 連線名稱 連線位置
+# git remote add origin  https://github.com/yolanda975/0427.
+# --git push 連線名稱 分支名稱(一般預設為 master)
+# git push origin master
+# git pull origin master
+# git clone 位置 (本地沒有此資料夾)
+#
+# https://www.w3schools.com/python/matplotlib_markers.asp
+import matplotlib.pyplot as plt
+listx = [16, 26, 36, 44, 50, 11]
+listy = [1, 3, 5, 7, 9, 11]
+plt.plot(listx,listy, marker = 'o')
+# plt.plot(listx,listy, marker = 'o', ms = 20, mec = 'r')
+plt.show()
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# ypoints = np.array([3, 8, 1, 10])
+#
+# plt.plot(ypoints, 'o:r')
+# plt.show()
+
